73da3eb465e9986664087d8176f777f11e714277
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-01-03 20:58:59 +0530

    delete button and edit read added

--- script.js
+++ script.js
@@ -15,32 +15,48 @@ function addBookToLibrary(){
     let pages = parseInt(document.getElementById("pages").value);
     let read = document.getElementById("yes").checked==true ? true : false;
 
-    const book = new Book(title, author, pages, read, globalId);
+    const book = new Book(title, author, pages, read, globalId++);
     myLibrary.push(book);
 
     console.log(myLibrary);
     addToDom(myLibrary);
 }   
 
-function deleteBook(){
+function deleteBook(id){
+    myLibrary.forEach(element => {
+        if(element.id==id){
+            myLibrary.splice(0,id);
+            addToDom(myLibrary)
+        }
+    });
+}
 
+function editRead(id){
+    myLibrary.forEach(element => {
+        if(element.id==id){
+            if(element.read==true){
+                element.read = false;
+            }else{
+                element.read = true;
+            }
+            addToDom(myLibrary);
+        }
+    });
 }
 
 function addToDom(myLibrary){
+    document.getElementById("table-body").innerHTML = "";
     myLibrary.forEach(element => {
-        let editRead = document.createElement("button");
-        editRead.setAttribute(`onclick`,`editRead(${element.id})`);
-        let deleteBook = document.createElement("button");
-        deleteBook.setAttribute(`onclick`,`deleteBook(${element.id})`);
-
+        
         let child = document.createElement("tr");
         child.innerHTML = `
             <td>${element.title}</td>
             <td>${element.author}</td>
             <td>${element.pages}</td>
             <td>${element.read==true?"Yes":"No"}</td>
-            <td>${editRead}</td>
-            <td>${deleteBook}</td>
+            <td><button onClick="editRead(${element.id})">Change Read status</button></td>
+            <td><button onClick="edit(${element.id})">Edit</button></td> 
+            <td><button onClick="deleteBook(${element.id})">Delete</button></td>
         `
         document.getElementById("table-body").appendChild(child);
     });
